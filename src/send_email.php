ca674b15f997670d2729e6d2ceeff6f3c6a89566
pejmang <pejman.gohari@gmail.com>
2024-09-09 13:22:58 +0200

    Update send_email.php

--- src/send_email.php
+++ src/send_email.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
     
     // Email configuration
-    $to = "your-email@example.com"; // Replace with your email
+    $to = "pejman.gohari@orbii.tech"; // Replace with your email
     $subject = "New Insight Builder Application";
     $headers = "From: " . $email . "\r\n";
     $headers .= "Reply-To: " . $email . "\r\n";
